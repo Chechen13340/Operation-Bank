bddac1cc8c27aac8aa1d13cc8906614b08f1571a
Artem Osipov <tema.osipov@mail.ru>
2023-07-02 21:07:34 +0300

    final update

---
+++ main.py
@@ -0,0 +1,14 @@
+from app.utils import get_operations, executed_operations, last_five_operations, sort_time_operation, \
+    display_information
+from settings import OPEARTION_PATH
+
+
+operations = get_operations(OPEARTION_PATH)
+ex_operations = executed_operations()
+five_ex_operations = last_five_operations()
+sort_for_date = sort_time_operation()
+output_information = display_information()
+
+
+for number in output_information:
+    print(number)

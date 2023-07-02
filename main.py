b9ce1e2a2520417d18793a59f0b3d89d420036bd
Artem Osipov <tema.osipov@mail.ru>
2023-07-02 20:21:36 +0300

    change import settings

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from app.utils import get_operations, executed_operations, last_five_operations, sort_time_operation, \
+    display_information
 from settings import OPEARTION_PATH
-from utils import get_operations, executed_operations, sort_time_operation, last_five_operations, display_information
+
 
 operations = get_operations(OPEARTION_PATH)
 ex_operations = executed_operations()

29f66cf6fa9dd4d97f69e2e49e25f8c1440d4b3f
Artem Osipov <tema.osipov@mail.ru>
2023-07-04 21:40:50 +0300

    add arguments for functions

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from settings import OPEARTION_PATH
 
 
 operations = get_operations(OPEARTION_PATH)
-ex_operations = executed_operations()
-five_ex_operations = last_five_operations()
-sort_for_date = sort_time_operation()
-output_information = display_information()
+ex_operations = executed_operations(operations)
+five_ex_operations = last_five_operations(ex_operations)
+sort_for_date = sort_time_operation(five_ex_operations)
+output_information = display_information(sort_for_date)
 
 
 for number in output_information:
